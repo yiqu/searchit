e2eeb909f7d3ef33b77c4ae34a3b30fd56581eff
Kevin Qu <kqu@praxislan01.com>
2017-01-24 08:45:17 -0500

    init commit

---
+++ src/client/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component , OnInit } from '@angular/core';
+import { Config } from './shared/config/env.config';
+import './operators';
+
+/**
+ * This class represents the main application component.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-app',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+
+  constructor() {
+    console.log('Environment config', Config);
+  }
+}
